7397b35830bbd7173b68f2de9544c163bb45ac16
wingshope <138878860+wingshope@users.noreply.github.com>
2023-11-16 20:44:12 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -40,7 +40,7 @@ res1() {
 netfilter-persistent
 clear
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[1;97;101m          UPDATE SCRIPT TekiroVPN       \e[0m"
+echo -e " \e[1;97;101m          UPDATE SCRIPT WingsVPN       \e[0m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e "  \033[1;91m update script service\033[1;37m"
